35e1262884a96b33ebd8f46d1b83978194247216
Richie Permana <richie.permana@gmail.com>
2020-03-04 00:32:09 +0800

    Added required attribute

--- api/models/product.js
+++ api/models/product.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  price: Number
+  name: { type: String, required: true },
+  price: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Product", productSchema);
